f9d460e6a127663a8cf69cc0f665c3d0eb7b057d
Benoit Dinocourt <ghrind@gmail.com>
2016-02-12 11:24:34 +0100

    Ignore merge commits when comparing two branches

--- lib/gitgut/git.rb
+++ lib/gitgut/git.rb
@@ -5,7 +5,7 @@ module Gitgut
   # git log --no-merges HEAD --not develop --pretty=oneline
   module Git
     def self.missing_commits_count(from, to)
-      `git rev-list #{from}..#{to} --count`.to_i
+      `git rev-list #{from}..#{to} --count --no-merges`.to_i
     end
   end
 end
